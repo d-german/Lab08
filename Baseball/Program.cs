177e315d99f4faeb596cfb85a0c9a1448730fa6f
d-german <facmet107@gmail.com>
2018-11-12 20:19:37 -0600

    Made more changes

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -5,32 +5,50 @@ namespace Baseball
 {
     internal class Program
     {
+        static readonly BaseballEntities Db = new BaseballEntities();
+
         private static void Main(string[] args)
         {
-            var db = new BaseballEntities();
+            //display the database
+            DisplayPlayers();
 
-            var jack = db.Players.FirstOrDefault(p => p.FirstName.Equals("Jack"));
-            if (jack != null) jack.LastName = "ZZZZ";
+            //find and update a player
+            var existingPlayer = Db.Players.FirstOrDefault(p => p.FirstName.Equals("Jack"));
+            if (existingPlayer != null) existingPlayer.LastName = "ZZZZ";
 
-            var player = new Player {FirstName = "George", LastName = "Brett", BattingAverage = .999m};
+            //Add a new player
+            var newPlayer = new Player {FirstName = "George", LastName = "Brett", BattingAverage = .999m};
+            Db.Players.Add(newPlayer);
 
-            db.Players.Add(player);
-            db.SaveChanges();
+            //Must save changes to the database
+            Db.SaveChanges();
 
-            foreach (var dbPlayer in db.Players)
+            //display the database, note the new player
+            DisplayPlayers();
+
+            //Remove the previously added player
+            Db.Players.Remove(newPlayer);
+            //Must save the changes to the database
+            Db.SaveChanges();
+
+            //Display the database, note the new player is gone.
+            DisplayPlayers();
+
+            DisplayPlayers(p => p.LastName == "ZZZZ");
+        }
+
+        private static void DisplayPlayers(Func<Player, bool> predicate = null)
+        {
+            Console.WriteLine();
+
+            predicate = predicate ?? (p => p != null);
+
+            foreach (var dbPlayer in Db.Players.Where(predicate))
             {
-                Console.WriteLine($"{dbPlayer.FirstName} {dbPlayer.LastName} {dbPlayer.BattingAverage}");
+                Console.WriteLine($"\t{dbPlayer.FirstName,-10} {dbPlayer.LastName,-10} {dbPlayer.BattingAverage,-10}");
             }
 
             Console.WriteLine();
-
-            db.Players.Remove(player);
-            db.SaveChanges();
-
-            foreach (var dbPlayer in db.Players)
-            {
-                Console.WriteLine($"{dbPlayer.FirstName} {dbPlayer.LastName} {dbPlayer.BattingAverage}");
-            }
         }
     }
 }
