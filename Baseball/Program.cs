b75ba1364263666ddfe6a2eac48cf3cf52390e3f
d-german <facmet107@gmail.com>
2018-11-12 19:53:44 -0600

    Made some changes

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -12,13 +12,25 @@ namespace Baseball
             var jack = db.Players.FirstOrDefault(p => p.FirstName.Equals("Jack"));
             if (jack != null) jack.LastName = "ZZZZ";
 
-            var player = new Player {FirstName = "George", LastName = "Bret", BattingAverage = .999m};
-            db.Players.Add(player);
+            var player = new Player {FirstName = "George", LastName = "Brett", BattingAverage = .999m};
 
+            db.Players.Add(player);
             db.SaveChanges();
 
             foreach (var dbPlayer in db.Players)
+            {
                 Console.WriteLine($"{dbPlayer.FirstName} {dbPlayer.LastName} {dbPlayer.BattingAverage}");
+            }
+
+            Console.WriteLine();
+
+            db.Players.Remove(player);
+            db.SaveChanges();
+
+            foreach (var dbPlayer in db.Players)
+            {
+                Console.WriteLine($"{dbPlayer.FirstName} {dbPlayer.LastName} {dbPlayer.BattingAverage}");
+            }
         }
     }
 }
